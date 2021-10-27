528944c8841a35d5a91442dbe7b68c3fc772bcb9
Daniel <daniel.esteban.8@hotmail.com>
2021-10-26 23:10:17 -0600

    Revert "Update noise_generator.m"
    This reverts commit 4f7ebdfb946351dc182791ffcf67bb1ead4484cc.

--- tarea2/parte1/noise_generator.m
+++ tarea2/parte1/noise_generator.m
@@ -4,30 +4,25 @@ close all;
 pkg load image
 pkg load video
 
-V = VideoReader('paid.mp4')
+V = VideoReader('video_inicial.mp4')
 fr = V.NumberOfFrames
 m = V.Height
 n = V.Width
 
-Y = uint8(zeros(m,n,3,fr));
+Y = zeros(m,n,3,fr);
 
 % Procesamiento video
-for k = 1:50;
-
+for k = 1:fr
   ZI = readFrame(V);
-  Y(:,:,1,k) = 255 - ZI(:,:,1);
-  Y(:,:,2,k) = 255 - ZI(:,:,2);
-  Y(:,:,3,k) = 255 - ZI(:,:,3);
-
-##  Y(:,:,:,k) = imnoise(ZI(:,:,:,k),"gaussian");
-  imshow(uint8(Y(:,:,:,k)));
-  pause(0.1);
+  Y(:,:,1:K) = 255 - ZI(:,:,1);
+  Y(:,:,2:K) = 255 - ZI(:,:,2);
+  Y(:,:,3:K) = 255 - ZI(:,:,3);
 endfor
 
 % Guardar Video
-##video = VideoWriter('video_con_ruido.mp4');
-##for i=1:fr
-##  writeVideo(video, Y(:,:,i));
-##endfor
-##
-##close(video);
+video = VideoWriter('video_con_ruido.mp4');
+for i=1:fr
+  writeVideo(video, Y(:,:,i));
+endfor
+
+close(video);
