9e7915ad150b0b5e014b87d4044f3db409afa0bf
Daniel <daniel.esteban.8@hotmail.com>
2021-10-27 01:23:31 -0600

    video con ruido

--- tarea2/parte1/noise_generator.m
+++ tarea2/parte1/noise_generator.m
@@ -9,25 +9,27 @@ fr = V.NumberOfFrames
 m = V.Height
 n = V.Width
 
-Y = uint8(zeros(m,n,3,fr));
+new_m = 320
+new_n = 480
+
+Y = uint8(zeros(new_m,new_n,1,fr));
+%Y = uint8(zeros(m,n,3,fr));
 
 % Procesamiento video
-for k = 1:50;
-
-  ZI = readFrame(V);
-  Y(:,:,1,k) = 255 - ZI(:,:,1);
-  Y(:,:,2,k) = 255 - ZI(:,:,2);
-  Y(:,:,3,k) = 255 - ZI(:,:,3);
-
-##  Y(:,:,:,k) = imnoise(ZI(:,:,:,k),"gaussian");
+for k = 1:fr-15;
+  ZI = rgb2gray(imresize(readFrame(V),[new_m,new_n]));
+ 
+##  Y(:,:,1,k) = 255 - ZI(:,:,1);
+##  Y(:,:,2,k) = 255 - ZI(:,:,2);
+##  Y(:,:,3,k) = 255 - ZI(:,:,3);
+  Y(:,:,k) = imnoise(ZI(:,:,:),"salt & pepper");
   imshow(uint8(Y(:,:,:,k)));
-  pause(0.1);
+  pause(0.001);
 endfor
 
 % Guardar Video
-##video = VideoWriter('video_con_ruido.mp4');
-##for i=1:fr
-##  writeVideo(video, Y(:,:,i));
-##endfor
-##
-##close(video);
+video = VideoWriter('video_con_ruido.mp4');
+for i=1:fr-15;
+  writeVideo(video, Y(:,:,1,i));
+endfor
+close(video);
