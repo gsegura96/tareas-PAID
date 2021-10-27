4f7ebdfb946351dc182791ffcf67bb1ead4484cc
Daniel <daniel.esteban.8@hotmail.com>
2021-10-26 23:09:29 -0600

    Update noise_generator.m

--- tarea2/parte1/noise_generator.m
+++ tarea2/parte1/noise_generator.m
@@ -4,25 +4,30 @@ close all;
 pkg load image
 pkg load video
 
-V = VideoReader('video_inicial.mp4')
+V = VideoReader('paid.mp4')
 fr = V.NumberOfFrames
 m = V.Height
 n = V.Width
 
-Y = zeros(m,n,3,fr);
+Y = uint8(zeros(m,n,3,fr));
 
 % Procesamiento video
-for k = 1:fr
+for k = 1:50;
+
   ZI = readFrame(V);
-  Y(:,:,1:K) = 255 - ZI(:,:,1);
-  Y(:,:,2:K) = 255 - ZI(:,:,2);
-  Y(:,:,3:K) = 255 - ZI(:,:,3);
+  Y(:,:,1,k) = 255 - ZI(:,:,1);
+  Y(:,:,2,k) = 255 - ZI(:,:,2);
+  Y(:,:,3,k) = 255 - ZI(:,:,3);
+
+##  Y(:,:,:,k) = imnoise(ZI(:,:,:,k),"gaussian");
+  imshow(uint8(Y(:,:,:,k)));
+  pause(0.1);
 endfor
 
 % Guardar Video
-video = VideoWriter('video_con_ruido.mp4');
-for i=1:fr
-  writeVideo(video, Y(:,:,i));
-endfor
-
-close(video);
+##video = VideoWriter('video_con_ruido.mp4');
+##for i=1:fr
+##  writeVideo(video, Y(:,:,i));
+##endfor
+##
+##close(video);
